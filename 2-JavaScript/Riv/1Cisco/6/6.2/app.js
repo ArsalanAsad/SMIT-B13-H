45f0c57c9f9c40879276c874b458825dc5a937ae
unknown <arsalan.gazdar@gmail.com>
2025-05-07 04:21:33 +0500

    RBJS

--- 2-JavaScript/Riv/1Cisco/6/6.2/app.js
+++ 2-JavaScript/Riv/1Cisco/6/6.2/app.js
@@ -52,8 +52,108 @@
 
 //6.2.5 Use of the debugger statement................................................................
 
-console.log("before outer() call");
-debugger;
-console.log(outer());
-console.log("after outer() call");
+// console.log("before outer() call");
+// debugger;
+// console.log(outer());
+// console.log("after outer() call");
 
+//6.2.6 Resume execution..............................................................................
+
+//6.2.10 Call stack...................................................................................
+// console.log(name); // -> inner
+
+// console.log(name); // -> outer
+
+//Viewing and modifying variables......................................
+// console.log(name); // -> inner
+// name = "new name";
+// console.log(name); // -> new name
+
+//6.2.11 Step Out......................................................................................
+// let part = 0;
+// for (let k = 0; k < 10000000; k++) {
+//     part = part + ((-1) ** k) / (2 * k + 1);
+// }
+// let pi = part * 4;
+// console.log(pi); // -> 3.1415925535897915
+
+// let part = 0;
+// console.time('Leibniz');
+// for (let k = 0; k < 10000000; k++) {
+//     part = part + ((-1) ** k) / (2 * k + 1);
+// }
+// console.timeEnd('Leibniz'); // -> Leibniz: 456.60498046875 ms
+// let pi = part * 4;
+// console.log(pi); // -> 3.1415925535897915
+
+// let part = 0;
+// console.time('Leibniz');
+// for(let k = 0; k < 10000000; k++) {
+//     part = part + (k % 2 ? -1 : 1) / (2 * k + 1);
+// }
+// console.timeEnd('Leibniz'); // -> Leibniz: 175.5458984375 ms
+// let pi = part * 4;
+// console.log(pi);
+
+//6.2.13 SECTION PRACTICE.....................................................................
+
+//Excercise01
+// let end = 2; 
+// for(let i=1; i
+//     console.log(i); 
+// }
+
+// let end = 6; 
+// for (let i = 1; i < end; i++) {
+//     console.log(i); 
+// }
+
+//Excercise02
+// let counter = 0; 
+// let maxValue = 10; 
+// let result = 1; 
+ 
+// debugger; 
+// for (counter = 0; counter < maxValue; counter++) { 
+//     console.log(result); 
+//     result *= maxValue - counter - 1; 
+// } 
+ 
+// console.log("Final result: ", result);
+
+// for (counter = 0; counter < maxValue; counter++) {
+//     console.table({
+//         counter,
+//         multiplier: maxValue - counter - 1,
+//         result
+//     });
+//     result *= maxValue - counter - 1;
+// }
+
+//Excercise03
+// function max(array) { 
+//     let maxValue = array[1]; 
+//     for(let i=1; i
+//         if(array[i] > maxValue) { 
+//             maxValue = array[i]; 
+//         } 
+//     } 
+//     return maxValue; 
+// } 
+ 
+// console.log( max([1, 4, 6, 2])); // -> 6 
+// console.log( max([10, 4, 6, 2])); // -> 6
+
+//Solutions
+function max(array) {
+    let maxValue = array[0]; // Start from the first element
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxValue) {
+            maxValue = array[i];
+        }
+    }
+    return maxValue;
+}
+
+console.log(max([1, 4, 6, 2]));     // -> 6
+console.log(max([10, 4, 6, 2]));    // -> 10 ✅
