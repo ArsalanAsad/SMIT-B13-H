316b110e2fdbe505a8e3af9881eb9eb8a03bb51e
unknown <arsalan.gazdar@gmail.com>
2025-05-09 03:47:38 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/6/Final Tes/app.js
@@ -0,0 +1,84 @@
+// let x = 2;
+// var y = 5;
+// const z = 10;
+// {
+//   let x = 3e-3;
+//   var y = 100;
+//   let a = 0.001;
+//   const z = 1000;
+// }
+// console.log(x + y + z);
+
+// let animals =["canary", "dog", "cat"];
+// animals.push("hamster");
+// console.log(animals);
+
+// console.error("This is an error message");
+
+// let msg1 = 'hello';
+// let msg2 = msg1.slice(-1);
+// console.log(msg2 ? msg2 : msg2 + msg1);
+
+// let userInput = prompt("Enter your name:", "Guest");
+
+// let x = false || true;
+// let y = "true" && "false";
+// let z = false && true;
+// console.log(`${x} ${y} ${z}`);
+
+// let x = true;
+// let y = false;
+// let z = true;
+// let resultA = x && y;
+// let resultB = x || y;
+// let resultC = !x;
+// console.log(resultA, resultB, resultC);
+
+// let a = 20 + "10";
+// let b = 20 + +"10";
+// let c = 20 + -"10" + "10";
+// let d = "10" - "10" + "100";
+// let e = "A" - "B" + 0xA;
+// console.log(`${a}, ${b}, ${c}, ${d}, ${e}`);
+
+// let x = [10, 20, 30, 40];
+// let y = [50, 60];
+// x.reverse().push(y);
+// console.log(x.length);
+
+// let colors = ['red', 'green', 'blue'];
+// for (let c of colors) console.log(c);
+
+// let a = (n) => {
+//     return n > 2 ? n * a(n - 1) : 2
+// }
+// console.log(a(6));
+
+// let outerVar = 100;
+// {
+//   let innerVar1 = 200;
+//   let innerVar2 = 300;
+//   innerVar1 = innerVar1 + innerVar2;
+//   outerVar = innerVar1 + outerVar;
+//   console.log("innerVar1: " + innerVar1);
+//   console.log("innerVar2: " + innerVar2);
+// }
+// console.log("outerVar: " + outerVar);
+
+// const a = "hello";
+//     console.log(a.toUpperCase());
+// }
+// } catch (error) {
+//     console.log(a)
+//     } finally {
+//     console.log(a);
+// }
+
+const a = "hello";
+    console.log(a.toUpperCase());
+}
+} catch (error) {
+    console.log(a)
+    } finally {
+    console.log(a);
+}
