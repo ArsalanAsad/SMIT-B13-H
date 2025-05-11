c8886872e61994f22514afd7713af1ee6e84c90e
unknown <arsalan.gazdar@gmail.com>
2025-05-11 23:40:06 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/2/2.0/app.js
@@ -0,0 +1,15 @@
+//2.0.3 Declaring variables.............................................
+// var  height;
+// console.log(height);  //  ->  undefined
+// console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+
+// let  height;
+// console.log(height);  //  ->  undefined
+
+var  height;
+var  height;
+console.log(height);  //  ->  undefined
+
+let  height;
+let  height;  //  ->  Uncaught  SyntaxError:  Identifier  'height'  has  already  been  declared
+console.log(height);
