9008ec29c20554116a61e7acd3bd2a876bafaef3
unknown <arsalan.gazdar@gmail.com>
2025-05-19 02:09:35 +0500

    first React JavaScript

---
+++ 3-ReactJavaScript/1-firstProject/my-first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React World!</h1>
+      <p>My First React App</p>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
---
+++ 3-ReactJavaScript/1-firstProject/my-first-react-app/src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+function HelloWorld() {
+  return (
+    <div className="greeting">
+      <h2>Component Example</h2>
+      <p>This is my first component!</p>
+    </div>
+  )
+}
+
+export default HelloWorld
