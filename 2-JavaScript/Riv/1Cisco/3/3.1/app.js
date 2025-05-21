fa3872740f62fd7256eb5c7aa53923a8efbe8d3f
unknown <arsalan.gazdar@gmail.com>
2025-05-21 22:37:19 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/3/3.1/app.js
@@ -0,0 +1,36 @@
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+
+// Create an Object
+// const person = {};
+
+// // Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+// Create an Object
+// const person = new Object();
+
+// // Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+console.log(person.fullName())
+console.log(person["lastName"])
+console.log(person.firstName)
