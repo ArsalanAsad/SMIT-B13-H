d3bd9854fd56857d8fdc9b5088ce8d2a98eae313
unknown <arsalan.gazdar@gmail.com>
2025-05-25 03:16:19 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/3/3.3/app.js
@@ -0,0 +1,222 @@
+// Question 1
+// Analyze the code snippet:
+
+// let n = 2 * 3 ** 3 - 1;
+// console.log(n)
+
+// Question 2
+// The number 2 is stored in the n variable: let n = 2;. The command n = n*n*n is then called. This last command can be replaced by:
+
+
+// radio_button_unchecked
+// n *= 3;
+
+// radio_button_unchecked
+// n **= 3;
+
+// radio_button_unchecked
+// n **= n;
+
+// radio_button_checked
+// n ***= n;
+
+// Question 3
+// The string "12" has been written into the str variable: let str = "12";. Then, the following operation is performed: str = +str. As a result, the variable str will contain:
+
+
+// radio_button_checked
+// "12"
+
+// radio_button_unchecked
+// 12
+
+// radio_button_unchecked
+// NaN
+
+// radio_button_unchecked
+// "+12"
+
+// Question 4
+// Analyze the code snippet:
+
+// let n = 10;
+// let m = ++n;
+// Its execution will result in the following values in the variables n and m:
+
+
+// radio_button_unchecked
+// n: 11, m: 10
+
+// radio_button_unchecked
+// n: 10, m: 10
+
+// radio_button_unchecked
+// n: 11, m: 11
+
+// radio_button_unchecked
+// n: 10, m: 11
+
+// Question 5
+// The result of the operation !(true && false || true) will be:
+
+
+// radio_button_unchecked
+// false
+
+// radio_button_unchecked
+// true
+
+// radio_button_unchecked
+// null
+
+// radio_button_unchecked
+// 1
+
+// Question 6
+// The result of the operation 20 || 5 will be:
+
+
+// radio_button_unchecked
+// 5
+
+// radio_button_unchecked
+// true
+
+// radio_button_checked
+// 20
+
+// radio_button_unchecked
+// 25
+
+// Question 7
+// The result of the operation false || "false" will be:
+
+
+// radio_button_unchecked
+// 0
+
+// radio_button_unchecked
+// "false"
+
+// radio_button_checked
+// false
+
+// radio_button_unchecked
+// true
+
+// Question 8
+// Analyze the code snippet:
+// let nr = "1";
+// let x = (nr === 1);
+// let y = (nr == 1);
+// let z = (nr = 1);
+// After its execution, the variables x, y, and z will have the values:
+
+
+// radio_button_checked
+// x: true, y: false, z: 1
+
+// radio_button_unchecked
+// x: null, y: null, z: 1
+
+// radio_button_unchecked
+// x: 1, y: 1, z: 1
+
+// radio_button_unchecked
+// x: false, y: true, z: 1
+
+// Question 9
+// Which operator do we use if we want to check if two variables store the same values of exactly the same type?
+
+
+// radio_button_unchecked
+// =
+
+// radio_button_unchecked
+// ===
+
+// radio_button_unchecked
+// ==
+
+// radio_button_unchecked
+// !==
+
+// Question 10
+// The result of the comparison "abcd" > "Abcd" will be:
+
+
+// radio_button_checked
+// true
+
+// radio_button_unchecked
+// false
+
+// radio_button_unchecked
+// "abcd"
+
+// radio_button_unchecked
+// 1
+
+// Question 11
+// The result of the operation 3 * 4 > 20 – 15 will be:
+
+
+// radio_button_unchecked
+// -14
+
+// radio_button_unchecked
+// true
+
+// radio_button_unchecked
+// NaN
+
+// radio_button_unchecked
+// false
+
+// Question 12
+// The confirm method allows you to create a dialog box. What value does this method return when the user closes the window?
+
+
+// radio_button_unchecked
+// It depends on the option selected by the user.
+
+// radio_button_unchecked
+// The string entered by the user.
+
+// radio_button_unchecked
+// Always false.
+
+// radio_button_unchecked
+// Always true.
+
+// Question 13
+// The methods window.alert, window.confirm, and window.prompt are methods of the window object. Which of the following is not true?
+
+// radio_button_unchecked
+// You can call window methods, such as window.alert, without including the name window, so calling alert("abc") would be correct.
+
+// radio_button_unchecked
+// The alert, confirm, and prompt methods require an argument specifying the position of the dialog box in which the information will be displayed.
+
+// radio_button_unchecked
+// The alert, confirm, and prompt methods display information in modal windows that block access to the page until they are closed.
+
+// radio_button_checked
+// The window object represents the window in which the HTML document containing the JavaScript code currently being executed is open.
+
+// Question 14
+// Analyze the following code:
+
+// let test = prompt("Hello", "World");
+
+// What value will the test variable have if, after running the code, we immediately press the OK button on the newly created dialog?
+// "OK"
+
+// radio_button_unchecked
+// "World"
+
+// radio_button_unchecked
+// true
+
+// radio_button_unchecked
+// "Hello"
