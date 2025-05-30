1a0d435cce6a639adc9a0c51e1472269cacc28fb
unknown <arsalan.gazdar@gmail.com>
2025-05-31 03:38:07 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/4/4.1/app.js
@@ -0,0 +1,47 @@
+//4.1.2 The while loop........................
+// console.log(0); // -> 0
+// console.log(10); // -> 10
+// console.log(20); // -> 20
+// console.log(30); // -> 30
+// console.log(40); // -> 40
+// console.log(50); // -> 50
+// console.log(60); // -> 60
+// console.log(70); // -> 70
+// console.log(80); // -> 80
+// console.log(90); // -> 90
+
+// let n = 0;
+ 
+// console.log(n); // -> 0
+// n += 10;
+// console.log(n); // -> 10
+// n += 10;
+// console.log(n); // -> 20
+// n += 10;
+// console.log(n); // -> 30
+// n += 10;
+// console.log(n); // -> 40
+// n += 10;
+// console.log(n); // -> 50
+// n += 10;
+// console.log(n); // -> 60
+// n += 10;
+// console.log(n); // -> 70
+// n += 10;
+// console.log(n); // -> 80
+// n += 10;
+// console.log(n); // -> 90
+// n += 10;
+
+// console.log(n);
+// n += 10;
+
+let n = 0;
+while(n < 91) {
+    console.log(n); // -> 0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+    n += 10;
+}
+
+// while(condition) {
+//     block of code
+// }
