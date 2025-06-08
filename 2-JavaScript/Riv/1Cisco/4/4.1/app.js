95e85b3a680fb677c71c095748364bea839eb70f
unknown <arsalan.gazdar@gmail.com>
2025-06-09 01:22:17 +0500

    RBJS

--- 2-JavaScript/Riv/1Cisco/4/4.1/app.js
+++ 2-JavaScript/Riv/1Cisco/4/4.1/app.js
@@ -327,9 +327,91 @@
 
 //4.1.11 SECTION PRACTICE.................................................................
 
+//.................................................
 //Exercise 1: Write a piece of code that will write numbers from 100 to 0 to the console, but in steps of 10; so 100, 90, 80... etc.
 
-for (let i = 100; i >= 0; i -= 10) {
-    console.log(i);
-}
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
 
+//.................................................
+//Exercise 2: Modify the previous program so that it asks the user for the first and last number it uses instead of 100 and 0 (hint: use the prompt dialog).
+
+// Check if the entered values are correct (that the initial value is greater than the final value).
+
+// let upperLimit = Number(prompt("Enter upper limit"));
+// let lowerLimit = Number(prompt("Enter lower limit"));
+
+// if (!Number.isNaN(upperLimit) && !Number.isNaN(lowerLimit) && upperLimit > lowerLimit) {
+//     for (let i = upperLimit; i >= lowerLimit; i -= 10) {
+//         console.log(i);
+//     }
+// } else {
+//     alert("Invalid input. Make sure the upper limit is greater than the lower limit and both are numbers.");
+// }
+
+//.....................................
+//Exercise 3: There are ten different numbers in the following numbers array:
+
+
+// let numbers = [21, 45, 100, 12, 11, 78, 61, 4, 39, 22];
+
+
+// Write a program that first writes out all these numbers on the console, then only those that are even (hint: the remainder of dividing an even number by 2 is equal to 0), then only those that are larger than 10 and at the same time smaller than 60.
+
+// 1. Print all numbers
+// console.log("all numbers");
+// for (let number of numbers) {
+//     console.log(number);
+// }
+
+// 2. Print only even numbers
+// console.log("even numbers");
+// for (let number of numbers) {
+//     if (number % 2 == 0) {
+//         console.log(number);
+//     } else console.log ("odd numbers");
+// }
+
+// 3. Print numbers > 10 and < 60
+// console.log("all numbers that are greater than 10 and less than 60");
+// for (let number of numbers) {
+//     if (number > 10 && number < 60) {
+//         console.log(number);
+//     }
+// } 
+
+//.................................................
+// Exercise 4: Write a program using a loop that will ask the user for the name of the movie (first prompt) and its rating from www.imdb.com (second prompt). The program will allow you to enter as many movies as you want into the movies array. Each element of the array will be an object, consisting of two fields: title and imdb. The input is completed if the user presses Cancel in the prompt dialog. Then the program should first print out to the console all movies that have a rating of less than 7, then those whose rating is greater than or equal to 7. Write the name of the movie and its rating next to each other, e.g.:
+
+
+// Lost in Translation (7.7)
+
+let movies = [];
+
+while (true) {
+    let title = prompt("Enter movie name");
+    let rating = prompt("Enter movie rating('imbd')");
+
+    if (title === null || rating === null) {
+        break;
+    } else {
+        movies.push({
+            title: title,
+            rating: Number(rating)
+        });
+    }
+    console.log("All with rating under 7;");
+    for (let movie of movies) {
+        if (movie.rating < 7) {
+            console.log(`${movie.title} (${movie.rating})`);
+        }
+    }
+
+    console.log("All movies with rating 7 or higher;");
+    for (let movie of movies) {
+        if(movie.rating >=7) {
+            console.log(`${movie.title} (${movie.rating})`);
+        }
+    }
+};
