73def41e1ed203893afc7f4016eb4829aecaa7bd
unknown <arsalan.gazdar@gmail.com>
2025-06-16 23:57:39 +0500

    RBJS

---
+++ 2-JavaScript/Riv/1Cisco/4/4.2/app.js
@@ -0,0 +1,98 @@
+///4.2 MODULE TEST...........................
+// Question 1
+// The condition if( a >= 0 ) can be replaced by the condition:
+
+
+// radio_button_unchecked
+// if (!(a > 0 && a == 0));
+
+// radio_button_unchecked
+// if (0 < a);
+
+// radio_button_unchecked
+// if (a > 0 || a == 0); //RIGHT ANSWER
+
+// radio_button_unchecked
+// if (a > 0 && a == 0);
+
+// Question 2
+// The condition if (!a) can be replaced by the condition:
+
+
+// radio_button_unchecked
+// if (a === false);
+
+// radio_button_unchecked
+// if (a);
+
+// radio_button_unchecked
+// if (!!a);
+
+// radio_button_unchecked
+// if (a == false); //RIGHT ANSWER
+
+// Question 3
+// We want to rewrite the following code snippet using the conditional operator:
+
+
+// let name;
+// if (test) {
+//     name = "Alice";
+// } else {
+//     name = "Bob";
+// }
+
+// Which notation is correct?
+
+
+// radio_button_unchecked
+// let name = (test)("Alice")("Bob");
+
+// radio_button_unchecked
+// let name = test ? "Alice" : "Bob";
+
+// radio_button_unchecked
+// let name = test : "Alice" ? "Bob";
+
+// radio_button_unchecked
+// let name = if test ? "Alice" : "Bob"; //RIGHT ANSWER
+
+// Question 4
+// Which of the following operators is a ternary one?
+
+
+// radio_button_unchecked
+// A typeof operator.
+
+// radio_button_unchecked
+// A conditional operator ? :. //RIGHT ANSWER
+
+// radio_button_unchecked
+// An increment operator ++.
+
+// radio_button_unchecked
+// An assignment operator =.
+
+// Question 5
+// Review the following code:
+
+
+// let x = 100;
+// if (x < 100)
+//     x = 20;
+// console.log(x)
+
+// What will be displayed in the console as a result of its execution?
+
+
+// radio_button_unchecked
+// nothing
+
+// radio_button_unchecked
+// 100
+
+// radio_button_unchecked
+// 20
+
+// radio_button_unchecked
+// false
