9d9ba0dc2719d8648f06e444966ae348c6c66a5d
unknown <arsalan.gazdar@gmail.com>
2025-06-19 23:57:57 +0500

    RBJS

--- 2-JavaScript/Riv/1Cisco/5/5.0/app.js
+++ 2-JavaScript/Riv/1Cisco/5/5.0/app.js
@@ -175,6 +175,28 @@
 
 //5.0.7 Shadowing...............................................................................................
 
+// function add(first, second) {
+//     return first + second;
+// }
+// let first = 10, second = 20, third = 40, fourth = 80;
+// console.log(add(first, second)); // -> 30
+// console.log(add(second, third)); // -> 60
+// console.log(add(third, fourth)); // -> 120
+
+// let a = 100, b = 200, c = 300;
+// function test(a) {
+//     let b = 10;
+//     console.log(a); // parameter a
+//     console.log(b); // local variable b
+//     console.log(c); // global variable c
+// }
+// test(1);   // -> 1
+// // -> 10
+// // -> 300
+// console.log(a); // -> 100
+// console.log(b); // -> 200
+// console.log(c); // -> 300
+
 function add(first, second) {
     return first + second;
 }
